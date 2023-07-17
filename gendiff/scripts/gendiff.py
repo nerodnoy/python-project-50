e37c42f0780c9b725cc635fcd09692c348858ddd
Artem Iliushin <thatsforsure@mail.ru>
2023-07-17 15:59:21 +0300

    fixed gendiff.py and hid dist/

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,8 +2,8 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', help='Path to the first file for comparison.')
-    parser.add_argument('second_file', help='Path to the second file for comparison.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
     args = parser.parse_args()
 
 
