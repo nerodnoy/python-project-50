7c08c0c97e65796928f3aea9114218b13e110704
Artem Iliushin <thatsforsure@mail.ru>
2023-07-17 17:02:05 +0300

    modified gendiff.py w/ -f FORMAT optional arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,8 +4,8 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help="set format of output")
     args = parser.parse_args()
-
 
 if __name__ == '__main__':
     main()
