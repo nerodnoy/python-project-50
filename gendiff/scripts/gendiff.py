a7a6e80cf68635630e3e64bdfa3ef95d5bfc119f
Artem Iliushin <thatsforsure@mail.ru>
2023-07-20 17:07:23 +0300

    rearrange files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ from gendiff.cli import get_parser_args
 
 def main():
     args = get_parser_args()
-    diff = generate_diff(args.first_file, args.second_file, args.format)
+    diff = generate_diff(args.first_file, args.second_file)
     print(diff)
 
 
